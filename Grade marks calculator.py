b3df4cb744f9340901fce454b8d1b533fc435d2f
Manixene <manishthakur01x@gmail.com>
2026-09-26 19:14:28 +0530

    Grade Calculate

---
+++ Grade marks calculator.py
@@ -0,0 +1,10 @@
+# Marks
+Marks =int(input("Enter your total Marks: "))
+
+if(Marks>=95):
+    print("Congratulation."" Your Grade is A++")
+elif(Marks>=60):
+    print("Good.""Your Grade is B")
+else:
+    print("your Grade is C")
+
